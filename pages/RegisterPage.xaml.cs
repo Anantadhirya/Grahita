474351d059d435fd092f378abd3fcc9dcc86c207
Anantadhirya <anantadhirya@gmail.com>
2023-11-15 11:01:53 +0700

    feat: register page and register backend

--- pages/RegisterPage.xaml.cs
+++ pages/RegisterPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Grahita.components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,52 @@ namespace Grahita.pages
         public RegisterPage()
         {
             InitializeComponent();
+        }
+        private void check(string text, ref TextBlock errorText, string type, ref bool valid, string password = "")
+        {
+            string errorMessage;
+            if (text == "")
+            {
+                errorMessage = type + " tidak boleh kosong.";
+            }
+            else if (type == "Confirm Password" && text != password)
+            {
+                errorMessage = "Password tidak sesuai.";
+            }
+            else
+            {
+                errorMessage = "";
+            }
+
+            if(errorMessage != "")
+            {
+                valid = false;
+                errorText.Text = errorMessage;
+                errorText.Visibility = Visibility.Visible;
+            } else
+            {
+                errorText.Text = "";
+                errorText.Visibility = Visibility.Collapsed;
+            }
+        }
+        private void onRegister(object sender, RoutedEventArgs e)
+        {
+            bool valid = true;
+            check(Username.Text, ref UsernameError, "Username", ref valid);
+            check(Password.Password, ref PasswordError, "Password", ref valid);
+            check(ConfirmPassword.Password, ref ConfirmPasswordError, "Confirm Password", ref valid, Password.Password);
+            check(Kontak.Text, ref KontakError, "Kontak", ref valid);
+            check(Alamat.Text, ref AlamatError, "Alamat", ref valid);
+            if(valid)
+            {
+                using (var db = new GrahitaDBEntities())
+                {
+                    var user = new User { Name = Username.Text, Password = Password.Password, Contact = Kontak.Text, Location = Alamat.Text };
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    MessageBox.Show("Pendaftaran berhasil", "", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
         }
     }
 }
